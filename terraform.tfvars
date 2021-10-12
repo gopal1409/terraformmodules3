060330151b7f716fb39a254769d0ed9262e2ce83
gopal <gopal1409@gmail.com>
2021-10-11 23:59:43 -0700

    module in terraform

---
+++ terraform.tfvars
@@ -0,0 +1,5 @@
+bucket_name = "gopalbuck14"
+tags = {
+    Terraform = "true"
+    Environment = "dev"
+}
---
+++ version.tf
@@ -0,0 +1,22 @@
+#terraform setting block when i do terraform init. this block will download the plugin required to communicate with aws. 
+terraform {
+    required_providers {
+        aws = {
+            source = "hashicorp/aws"
+            #version = "~> 2.8" #but in production always providers a version
+        }
+    }
+}
+
+
+
+
+#provider block
+#once the plugin has been download in aws they have region specific plugin. 
+provider "aws" {
+  region = "us-east-1"
+  profile = "default"
+}
+
+
+
